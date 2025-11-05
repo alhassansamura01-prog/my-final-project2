d3968b821a943e4a6f6cdfb256be1278a5847feb
unknown <alhassansamura01@gmail.com>
2025-11-05 15:16:57 +0000

    update

---
+++ script.js
@@ -0,0 +1,23 @@
+// ===== MANUAL IMAGE SLIDER WITH NEXT & PREVIOUS BUTTONS =====
+let currentSlide = 0;
+
+function showSlide(index) {
+  const slides = document.querySelectorAll(".slides img");
+  const dots = document.querySelectorAll(".dot");
+
+  if (index >= slides.length) currentSlide = 0;
+  else if (index < 0) currentSlide = slides.length - 1;
+  else currentSlide = index;
+
+  slides.forEach((img, i) => {
+    img.classList.remove("active");
+    dots[i].classList.remove("active");
+  });
+
+  slides[currentSlide].classList.add("active");
+  dots[currentSlide].classList.add("active");
+}
+
+function changeSlide(direction) {
+  showSlide(currentSlide + direction);
+}
